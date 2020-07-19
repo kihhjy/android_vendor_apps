2d38629fe08f137657cda172aa7c63e4330bc8d0
kihhjy <2812339969@qq.com>
2020-07-19 09:21:57 +0800

    add googlepinyin

---
+++ googlepinyin/Android.mk
@@ -0,0 +1,44 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := googlepinyin
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+
+LOCAL_PREBUILT_JNI_LIBS:= \
+      @lib/arm/liben_data_bundle.so \
+      @lib/arm/libgnustl_shared.so \
+      @lib/arm/libhmm_gesture_hwr_zh.so \
+      @lib/arm/libhwrword.so \
+      @lib/arm/libpinyin_data_bundle.so \
+
+LOCAL_CERTIFICATE := PRESIGNED
+
+#LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_MODULE_CLASS := APPS
+
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+LOCAL_DEX_PREOPT := true
+
+LOCAL_OVERRIDES_PACKAGES := inputmethod
+
+include $(BUILD_PREBUILT)
